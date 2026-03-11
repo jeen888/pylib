e0a55c6cc647c20813d15122d4a9ffc478bb15d2
jeen888 <jeengeek2@gmail.com>
2026-03-11 11:12:13 +0900

    .

--- sendbox/035-pathlib.py
+++ sendbox/035-pathlib.py
@@ -1,7 +1,7 @@
 # 전통적 방식
 # import glob
 # import os
-import shutil
+# import shutil
 # 
 # for file_path in glob.glob('%s/*.py' % os.getcwd()):
 #     parent = os.path.dirname(file_path)
@@ -10,9 +10,15 @@ import shutil
 #     # shutil.move(file_path, new_path)
 #     shutil.copy(file_path, new_path)
 # 
-import pathlib
+# import pathlib
 
-for p in pathlib.Path.cwd().glob('*.py'):
-    new_p = p.parent.joinpath('archive', p.name)
-    # p.replace(new_p)
-    shutil.copy(p, new_p)
+# for p in pathlib.Path.cwd().glob('*.py'):
+#     new_p = p.parent.joinpath('archive', p.name)
+#     # p.replace(new_p)
+#     shutil.copy(p, new_p)
+
+# 현재 디재토렉ㅌ리모모든드 파일 확장자별 개수 
+import collections, pathlib
+
+counter = collections.Counter(p.suffix for p in pathlib.Path.cwd().iterdir() if p.is_file())
+print(counter)
